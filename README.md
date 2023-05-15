20da77a561a29f60cdee827f1455740fa51d82d9
prozhou <zhoushengzheng@gmail.com>
2023-05-15 19:08:46 +0800

    feat: insert data into sqlite3

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # boss 直聘爬虫
 ## 功能
 - [X] 爬取列表职位信息
+- [X] 爬取数据写入sqlite3数据库
 - [ ] 过滤已沟通职位
 - [ ] 自动投递
 
@@ -12,6 +13,16 @@ pip3 install selenium
 ## 使用
 ```bash
 python3 boss.py| tee out.txt
+```
+## sqlite3 客户端
+ubuntu 系统下，按张sqlite3 客户端
+
+```bash
+sudo add-apt-repository -y ppa:linuxgndu/sqlitebrowser
+sudo apt-get install sqlitebrowser
+sqlite3 db.sqlite3
+
+
 ```
 ## 注意
 使用测试号爬职位链接。用正式号打开职位链接投递。
--- boss.py
+++ boss.py
@@ -1,22 +1,59 @@
-
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+import sqlite3
+import datetime
+
+conn = sqlite3.connect("db.sqlite3")
+cursor=conn.cursor()
+
+# status 1, 入库  2, 已投递 3, 未投递
+#创建职位表
+cursor.execute("""
+CREATE TABLE if not exists jobs(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  job_name TEXT,
+  company_name TEXT,
+  salary TEXT,
+  detail_url TEXT,
+  uniq_url TEXT,
+  status INTEGER,
+  create_time timestamp,
+  apply_time  timestamp
+);
+""")
+
+cursor.execute("""CREATE INDEX if not exists idx_jobs_uniq_url_status ON jobs (uniq_url, status);
+""")
+
+conn.commit()
+
+
+def insertRow(job_name,company_name,salary,detail_url):
+  create_time = datetime.datetime.now()
+
+  sqlite_insert_query = """INSERT INTO jobs
+   (job_name,company_name , salary, detail_url, uniq_url, status, create_time)
+                             VALUES
+                            ("{}","{}","{}","{}","{}",{},"{}");"""
+
+  uniq_url = detail_url.split("?")[0]
+  sql = sqlite_insert_query.format(job_name,company_name,salary, detail_url, uniq_url, 1,create_time)
+  cursor.execute(sql)
+  conn.commit()
 
 
 driver = webdriver.Chrome()
-driver.get("https://www.zhipin.com")
-
-
 try:
+  driver.get("https://www.zhipin.com")
   # login
   element = WebDriverWait(driver,3000).until(
       EC.presence_of_all_elements_located((By.CLASS_NAME, "nav-figure"))
       )
 
   # 循环查询的职位列表
-  for i in range(1,8):
+  for i in range(1,3):
     url = "https://www.zhipin.com/web/geek/job?query=golang%E4%B8%BB%E7%A8%8B&city=101010100&salary=406&page={}"
     driver.get(url.format(i))
     WebDriverWait(driver,3000).until(
@@ -44,6 +81,7 @@ try:
       jobList.append(curE)
 
     for job in jobList:
+      insertRow(job["job_name"],job["company_name"],job["salary"],job["detail_url"])
       print("cur {}".format(i))
       print(job["detail_url"])
       print(job["job_name"])
